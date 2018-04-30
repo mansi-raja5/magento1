a55028e3599a70c6d6516c7efd9e8f353b76ce67
Mansi Raja <mansi.raja5@gmail.com>
2018-04-30 15:29:07 +0530

    extra commmit

--- app/code/local/Cybercom/Vendor/Block/Adminhtml/Vendors.php
+++ app/code/local/Cybercom/Vendor/Block/Adminhtml/Vendors.php
@@ -6,7 +6,7 @@ class Cybercom_Vendor_Block_Adminhtml_Vendors extends Mage_Adminhtml_Block_Widge
     {
         $this->_blockGroup = 'cybercom_vendor';
         $this->_controller = 'adminhtml_vendors';
-        $this->_headerText = Mage::helper('cybercom_vendor')->__('Cybercom Vendor Details');
+        $this->_headerText = "Cybercom Vendor Details";
 
         parent::__construct();
         //$this->_removeButton('add');
